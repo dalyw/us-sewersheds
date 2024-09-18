b3ae31337a53731604d84b26a79ec209ce8d0b13
Daly Wettermark <139182397+dalyw@users.noreply.github.com>
2024-09-18 15:08:12 -0700

    Consolidated sewersheds with redundant nodes

--- plot_sewersheds.py
+++ plot_sewersheds.py
@@ -40,18 +40,27 @@ def __(nx, plt):
         center_node = sewershed_map[sewershed_id]['center']
         facility_names = {node: f'{(facilities.loc[facilities['CWNS_ID'] == node, 'FACILITY_NAME'].iloc[0] if not facilities[facilities['CWNS_ID'] == node].empty else str(node))}' for node in nodes}
         facility_permit_numbers = {node: f'{(facilities.loc[facilities['CWNS_ID'] == node, 'PERMIT_NUMBER'].iloc[0] if not facilities[facilities['CWNS_ID'] == node].empty else str(node))}' for node in nodes}
-        node_labels = {node: f'{facility_names[node]}\n({facility_permit_numbers[node]})' for node in nodes}
+        node_labels = {node: f'{facility_names[node]}\nPermit no: {facility_permit_numbers[node]}' for node in nodes}
+
         G.add_nodes_from(nodes)
         G.add_edges_from([(conn[0], conn[1], {'label': f'{conn[2]}%'}) for conn in connections])
-        # pos = nx.spring_layout(G)
         pos = nx.spring_layout(G, k=3, fixed={center_node: (0, 0)}, pos={center_node: (0, 0)})
         plt.figure(figsize=(9, 6))
         nx.draw_networkx_edge_labels(G, pos, edge_labels=nx.get_edge_attributes(G, 'label'))
-        nx.draw(G, pos, labels=node_labels, with_labels=True, node_size=3000, node_color='lightblue', font_size=6, font_weight='bold', edge_color='gray', alpha=0.7, arrows=True)
+
+        node_shapes = {node: 's' if any('Treatment' in facility_type for facility_type in facilities.loc[facilities['CWNS_ID'] == node, 'FACILITY_TYPE']) else '2' for node in nodes}
+        for shape in set(node_shapes.values()):
+            node_list = [node for node in nodes if node_shapes[node] == shape]
+            nx.draw_networkx_nodes(G, pos, nodelist=node_list, node_shape=shape, node_size=3000, node_color='lightblue', alpha = 0.7)
+
+        
+        nx.draw_networkx_edges(G, pos, edge_color='gray', alpha=0.7, arrows=True, arrowsize=20, arrowstyle='->', min_source_margin=30, min_target_margin=30)
+        nx.draw_networkx_labels(G, pos, labels=node_labels, font_size=6, font_weight='bold')
+
         plt.title(f'{sewershed_id}')
         plt.axis('off')
         plt.tight_layout()
-        return plt
+        return plt, G, pos
     return add_connection, plot_sewershed
 
 
@@ -62,6 +71,10 @@ def __(pd):
     facility_permit = pd.read_csv('CA_2022CWNS_APR2024/FACILITY_PERMIT.csv', encoding='latin1', low_memory=False)
     facilities = pd.read_csv('CA_2022CWNS_APR2024/FACILITIES.csv', encoding='latin1', low_memory=False)
     areas_county = pd.read_csv('CA_2022CWNS_APR2024/AREAS_COUNTY.csv', encoding='latin1', low_memory=False)
+    facility_types = pd.read_csv('CA_2022CWNS_APR2024/FACILITY_TYPES.csv', encoding='latin1', low_memory=False)
+    population_wastewater = pd.read_csv('CA_2022CWNS_APR2024/POPULATION_WASTEWATER.csv', encoding='latin1', low_memory=False)
+    population_wastewater_confirmed = pd.read_csv('CA_2022CWNS_APR2024/POPULATION_WASTEWATER_CONFIRMED.csv', encoding='latin1', low_memory=False)
+    population_decentralized = pd.read_csv('CA_2022CWNS_APR2024/POPULATION_DECENTRALIZED.csv', encoding='latin1', low_memory=False)
     discharges = pd.read_csv('CA_2022CWNS_APR2024/DISCHARGES.csv', encoding='latin1', low_memory=False)
     discharges['DISCHARGES_TO_CWNSID'] = pd.to_numeric(discharges['DISCHARGES_TO_CWNSID'], errors='coerce').astype('Int64')
     discharges['CWNS_ID'] = pd.to_numeric(discharges['CWNS_ID'], errors='coerce').astype('Int64')
@@ -70,7 +83,17 @@ def __(pd):
     discharges = discharges.merge(facilities, left_on='CWNS_ID', right_index=True, how='left')
     discharges = discharges.merge(areas_county, left_on='CWNS_ID', right_on='CWNS_ID', how='left')
     facilities = facilities.merge(facility_permit, left_on='CWNS_ID', right_on='CWNS_ID', how='left')
-    return areas_county, discharges, facilities, facility_permit
+    facilities = facilities.merge(facility_types, left_on='CWNS_ID', right_on='CWNS_ID', how='left')
+    return (
+        areas_county,
+        discharges,
+        facilities,
+        facility_permit,
+        facility_types,
+        population_decentralized,
+        population_wastewater,
+        population_wastewater_confirmed,
+    )
 
 
 @app.cell
@@ -78,46 +101,72 @@ def __(add_connection, discharges, pd):
     # BUILD SEWERSHED MAP
 
     sewershed_map = {}
+    rows_new_sewershed = []
     facilities_already_mapped = []
-    for _, row in discharges[discharges['DISCHARGE_TYPE'] == 'Discharge To Another Facility'].iterrows():
-        cwns_id, discharges_to = row['CWNS_ID'], row['DISCHARGES_TO_CWNSID']
-        if cwns_id not in facilities_already_mapped and discharges_to not in facilities_already_mapped:
+    for _, row in discharges[
+        discharges["DISCHARGE_TYPE"] == "Discharge To Another Facility"
+    ].iterrows():
+        cwns_id, discharges_to = row["CWNS_ID"], row["DISCHARGES_TO_CWNSID"]
+        if (
+            cwns_id not in facilities_already_mapped
+            and discharges_to not in facilities_already_mapped
+        ):
+            rows_new_sewershed.append(row.name)
             new_sewershed_id = len(sewershed_map) + 1
             sewershed_map[new_sewershed_id] = {
-                'nodes': [cwns_id, discharges_to],
-                'connections': [add_connection(row)]
+                "nodes": set([cwns_id, discharges_to]),
+                "connections": [add_connection(row)],
             }
-            facilities_already_mapped.append(cwns_id)
-            facilities_already_mapped.append(discharges_to)
+            facilities_already_mapped.extend([cwns_id, discharges_to])
         else:
             for sewershed_info in sewershed_map.values():
-                if cwns_id in sewershed_info['nodes'] or discharges_to in sewershed_info['nodes']:
-                    if cwns_id not in sewershed_info['nodes']:
-                        sewershed_info['nodes'].append(cwns_id)
-                    if discharges_to not in sewershed_info['nodes']:   
-                        sewershed_info['nodes'].append(discharges_to)
-                    sewershed_info['connections'].append(add_connection(row))
-                    facilities_already_mapped.append(cwns_id)
-                    facilities_already_mapped.append(discharges_to)
+                if (
+                    cwns_id in sewershed_info["nodes"]
+                    or discharges_to in sewershed_info["nodes"]
+                ):
+                    sewershed_info["nodes"].update([cwns_id, discharges_to])
+                    sewershed_info["connections"].append(add_connection(row))
+                    facilities_already_mapped.extend([cwns_id, discharges_to])
                     break
-                    
+
+    # Consolidate sewersheds with redundant nodes
+    sewershed_ids = list(sewershed_map.keys())
+    for i in range(len(sewershed_ids)):
+        for j in range(i + 1, len(sewershed_ids)):
+            id1, id2 = sewershed_ids[i], sewershed_ids[j]
+            if id1 in sewershed_map and id2 in sewershed_map:
+                if sewershed_map[id1]["nodes"] & sewershed_map[id2]["nodes"]:
+                    # Merge sewersheds
+                    sewershed_map[id1]["nodes"].update(sewershed_map[id2]["nodes"])
+                    sewershed_map[id1]["connections"].extend(sewershed_map[id2]["connections"])
+                    del sewershed_map[id2]
+
+
     new_sewershed_map = {}
     name_used = {}
     for _sewershed_info in sewershed_map.values():
-        county_names = discharges.loc[discharges['CWNS_ID'].isin(_sewershed_info['nodes']), 'COUNTY_NAME']
-        primary_county = county_names.value_counts().index[0] if len(county_names.value_counts()) > 0 else 'Unspecified'
-        primary_county = 'Unspecified' if pd.isna(primary_county) else primary_county
+        county_names = discharges.loc[
+            discharges["CWNS_ID"].isin(_sewershed_info["nodes"]), "COUNTY_NAME"
+        ]
+        primary_county = (
+            county_names.value_counts().index[0]
+            if len(county_names.value_counts()) > 0
+            else "Unspecified"
+        )
+        primary_county = (
+            "Unspecified" if pd.isna(primary_county) else primary_county
+        )
         name_used[primary_county] = name_used.get(primary_county, 0) + 1
-        new_name = f'{primary_county} County Sewershed {name_used[primary_county]}'
+        new_name = f"{primary_county} County Sewershed {name_used[primary_county]}"
         connection_counts = {}
-        for node in _sewershed_info['nodes']:
+        for node in _sewershed_info["nodes"]:
             count = 0
-            for connection in _sewershed_info['connections']:
+            for connection in _sewershed_info["connections"]:
                 if connection[0] == node or connection[1] == node:
                     count += 1
             connection_counts[node] = count
         center = max(connection_counts.items(), key=lambda x: x[1])[0]
-        _sewershed_info['center'] = center
+        _sewershed_info["center"] = center
         new_sewershed_map[new_name] = _sewershed_info
     sewershed_map = new_sewershed_map
     return (
@@ -129,6 +178,10 @@ def __(add_connection, discharges, pd):
         cwns_id,
         discharges_to,
         facilities_already_mapped,
+        i,
+        id1,
+        id2,
+        j,
         name_used,
         new_name,
         new_sewershed_id,
@@ -136,6 +189,8 @@ def __(add_connection, discharges, pd):
         node,
         primary_county,
         row,
+        rows_new_sewershed,
+        sewershed_ids,
         sewershed_info,
         sewershed_map,
     )
@@ -149,7 +204,7 @@ def __(mo):
 
 @app.cell
 def __(mo):
-    keyword = mo.ui.text(label='Filter facility name (or portion of name): ')
+    keyword = mo.ui.text(label='Filter by facility name (or portion of name): ')
     keyword
     return keyword,
 
@@ -176,7 +231,7 @@ def __(mo, results_list):
 
 @app.cell
 def __(mo):
-    button = mo.ui.run_button(label = 'Generate Plot')
+    button = mo.ui.run_button(label = 'Generate Sewershed Plot')
     button
     return button,
 
@@ -184,14 +239,9 @@ def __(mo):
 @app.cell
 def __(button, dropdown, facilities, mo, plot_sewershed, sewershed_map):
     mo.stop(not button.value)
-    plot = plot_sewershed(dropdown.value, sewershed_map, facilities)
+    plot, G, pos = plot_sewershed(dropdown.value, sewershed_map, facilities)
     plot.gca()
-    return plot,
-
-
-@app.cell
-def __():
-    return
+    return G, plot, pos
 
 
 @app.cell
